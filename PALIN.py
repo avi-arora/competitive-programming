cc6fc20e3f1e8a750147531d2163b31f004fc3be
avishek arora <avishek.arora@icloud.com>
2016-09-28 12:48:56 +0530

    Working on newer imporved_palindrome, almost completed.

--- PALIN.py
+++ PALIN.py
@@ -1,7 +1,5 @@
 """
-	Problem description: A positive integer is called a palindrome,
-	 if its representation in the decimal system is the same when read from left to right and from right to left. 
-	 For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output.
+	Problem description: A positive integer is called a palindrome, if its representation in the decimal system is the same when read from left to right and from right to left. For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output.
 	 Numbers are always displayed without leading zeros.
 
 	 Inputs: The first line contains integer t, the number of test cases. Integers K are given in the next t lines.
@@ -20,52 +18,47 @@ def next_palindrome_brute_force(number):
 			if number == int(str(number)[::-1]):
 				return number
 def improved_palindrome(inputstr):
-	characters = []
-	for char in inputstr:
-		characters += [char]
-	center = len(characters) // 2
-	first = 0
-	last = -1
-	if inputstr != inputstr[::-1]: #if number is not palindrome.
-		while first < center: #make the number palindrome by mirroring
-			if characters[first] != characters[last]:
-				characters[last] = characters[first]
-			first += 1; last -= 1
-	
-	#if palindrome is bigger than inputstr, return it
-	palindrome = ''.join(characters)
+	#if number is not palindrome, make it palindrome by mirroring.
+	palindrome = left = middle = right = ''
+	center = len(inputstr) // 2
+	if inputstr != inputstr[::-1]:
+		if center % 2 != 0: # for even number
+			left = inputstr[:center-1]
+			middle = inputstr[center - 1] + inputstr[::-1][center:center + 1]
+			right = inputstr[::-1][center + 1:]
+		else: 
+			left = inputstr[:center]
+			middle = inputstr[center]
+			right = inputstr[::-1][center + 1:]
+	else:
+		if center % 2 != 0:
+			left = inputstr[:center - 1]
+			middle = inputstr[center -1] + inputstr[center:center + 1]
+			right = inputstr[center+1:]
+		else:
+			left = inputstr[:center]
+			middle = inputstr[center]
+			right = inputstr[center + 1:]
+	palindrome = left + middle + right
 	if palindrome > inputstr:
 		return palindrome
-	else:
-		if len(inputstr) % 2 != 0:
-			if characters[center] < '9':
-				characters[center] = str(int(characters[center]) + 1)
-				return ''.join(characters)
-			else:
-				characters[center] = '0'
-				for c in range(1, center+1):
-					if characters[center - c] < '9':
-						characters[center - c] = characters[center + c] = str(int(characters[center + c]) + 1)
-						return ''.join(characters)
-					characters[center - c] = characters[center + c] = '0'
-				characters[-1] = '1'
-				return '1' + ''.join(characters)
+	if center % 2 != 0:
+		if middle[0] < '9':
+			middle = str(int(middle[0]) + 1) * 2
+			palindrome = left + middle + right
+			return palindrome
 		else:
-			if characters[center] < '9' and characters[center - 1] < '9':
-				characters[center] = str(int(characters[center]) + 1)
-				characters[center-1] = str(int(characters[center-1]) + 1)
-				return ''.join(characters)
-			else:
-				for c in range(0,center+1):
-					if characters[center + c] < '9':
-						characters[center + c] = characters[center - c - 1] = str(int(characters[center + c]) + 1)
-						return ''.join(characters)
-					characters[center + c] = characters[center - c - 1] = '0'
-					characters[-1] = '1'
-				return '1' + ''.join(characters)
+			pass
+	else:
+		if middle < '9':
+			middle = str(int(middle) + 1)
+			palindrome = left + middle + right
+			return palindrome
+		else:
+			pass
+
 def run():
 	total_cases = input()
 	for _ in range(total_cases):
-		print int(improved_palindrome(raw_input()))
-run()
+		improved_palindrome(raw_input())
 	
