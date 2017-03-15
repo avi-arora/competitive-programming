cc3276fc51d83b7278ffc26d3528fab1251199c6
avishek arora <avishek.arora@icloud.com>
2017-03-16 03:36:14 +0530

    CHEFAPAR accepted, after minor i/o changes.

--- CHEFAPAR.py
+++ CHEFAPAR.py
@@ -1,33 +1,28 @@
 test_cases = int(input());
 for t in range(test_cases):
 	total_months = int(input())
-	statement = []
-	for month in range(total_months):
-		statement += [int(input())]
+	statement = list(map(int, input().split()))
 	#input ends.
-	total_unpaid_months = 0
-	total_unpaid_fine = 0
+
 	CHARGES = 1000
 	LATEFINE = 100
 
-	starting_index = 0
-	#special handling for starting consecutive one's (1's)
-	for i in range(len(statement)):
-		if statement[i] == 1:
-			starting_index += 1
-		else:
+	#remove the consecutive 1's untill 0 is encountered
+	first_zero = 0
+	for s in statement:
+		if s == 0:
 			break
+		first_zero += 1
 
-	for s in range(starting_index, len(statement)):
-		if statement[s] == 0:
-			total_unpaid_months += 1
-			total_unpaid_fine += 1
-		else:
-			total_unpaid_fine += 1
+	statement = statement[first_zero:] 
 
-	print((total_unpaid_months * CHARGES) + (total_unpaid_fine * LATEFINE))
+	#count the number of one's
+	total_one = 0
+	for s in statement:
+		if s == 1:
+			total_one += 1
 
-
+	print((len(statement) * 1100) - (total_one * CHARGES))
 
 
 
