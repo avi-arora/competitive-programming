ea12c2a63d030d1845fd53d9587be6fb481c9bb1
avishek arora <avishek.arora@icloud.com>
2017-05-11 04:59:26 +0530

    completed w/o U

--- LISDIGIT.py
+++ LISDIGIT.py
@@ -1,6 +1,8 @@
 test_cases = int(input())
 for t in range(test_cases):
 	digits = int(input())
-	LIS = [int(number) for number in input().split()]
+	LIS = [number for number in input().split()]
 	#input completed.
-	
+	#joining the input, 
+	print(''.join(LIS))
+
