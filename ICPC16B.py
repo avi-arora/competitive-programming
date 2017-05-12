40d3809ce5648e474657fd3ffb5101db2fa27158
avishek arora <avishek.arora@icloud.com>
2017-05-13 02:43:09 +0530

    Bruteforce soln, time limit exceed, need different approach, all previous approach fails, no binary search and sorting is required.

--- ICPC16B.py
+++ ICPC16B.py
@@ -1,18 +1,33 @@
 #user input
-test_cases = int(input())
-for test in range(test_cases):
-	length = int(input())
-	digits = [int(number) for number in input().split()]
-	#input completed
-	flag = 0
-	for i in range(len(digits) - 1):
-		product = digits[i] * digits[i+1];
-		if product in digits:
-			flag = 1
-			break
-	if flag == 1:
-		print("yes")
-	else:
-		print("no")
+def run():
+	test_cases = int(input())
+	for test in range(test_cases):
+		length = int(input())
+		digits = [int(number) for number in input().split()]
+		#input completed
+		#check for every pair of number,
+		i, flag = 0, 0
+		while i < length:
+			j = 0
+			while j < length:
+				if j == i:
+					j = j + 1
+					continue
+				else:
+					product = digits[i] * digits[j]
+					if product not in digits:
+						flag = 1
+						break
+					j = j + 1
+			if flag == 1:
+				break
+			i = i + 1
+		if flag == 1:
+			print("no")
+		else:
+			print("yes")
+run()
+
+		
 
 
