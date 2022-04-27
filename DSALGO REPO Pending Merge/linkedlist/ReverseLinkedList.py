7ca27fd74e6cd3875d1095b9302284ad87a73b16
avishek arora <avishek.arora@icloud.com>
2022-04-27 16:23:30 +0530

    improved ll

--- DSALGO REPO Pending Merge/linkedlist/ReverseLinkedList.py
+++ DSALGO REPO Pending Merge/linkedlist/ReverseLinkedList.py
@@ -1,83 +1,25 @@
-from SinglyLinkedList import SinglyLinkedList
-
-
-def reverseNaive(list: SinglyLinkedList):
-    """
-    using insert at front technique
-
-    TC: O(n)
-    SC: O(n) -  not so good
-
-    """
-    result, temp = SinglyLinkedList(), list
-    while temp:
-        result.insertAtFront(temp.getData())
-        temp = temp.getNext()
-    
-    return result
-
-def reverseUsingStack(list: SinglyLinkedList):
-    """
-    using stack push and pop operations
-    TC: O(n)
-    SC: O(n) - not improved
-    """
-    stack, temp = [], list
-    while temp: 
-        stack.append(temp.getData())
-        temp = temp.getNext()
-
-    temp = list
-    while temp:
-        temp.setData(stack.pop())
-        temp = temp.getNext()
-    
-    return list
-
-def reverseImproved(list: SinglyLinkedList):
-    """
-    TC: O(n)
-    SC: O(1)
-    """
-    temp, prev, current = list.getNext(), list, list.getNext()
-    while current:
-        temp = temp.getNext()
-        current.setNext(prev)
-        prev = current 
-        current = temp
-    list.setNext(None)
-    return SinglyLinkedList(prev)
-         
-
-def reverseRecursive(head, current, prev):
-    """
-    TC: O(n)
-    SC: O(n)
-    """
-    if not current.getNext():
-        current.setNext(prev)
-        return current
-    else:
-        newHead = reverseRecursive(head, current.getNext(), current)
-        current.setNext(prev)
-        if prev == head:
-            prev.setNext(None)
-            return SinglyLinkedList(newHead)
-        return newHead
-
-
-
-
-if __name__ == "__main__":
-
-    list = SinglyLinkedList()
-    list.insertAtLast(1)
-    list.insertAtLast(2)
-    list.insertAtLast(3)
-    list.insertAtLast(4)
-    list.insertAtLast(5)
-
-    list.print()
-    rev = reverseRecursive(list, list.getNext(), list)
-    rev.print()
-    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head):
+        prev, curr = head, head.next if head and head.next else None
+        
+        # Edge # if list only contains single node
+        if not curr:
+            return head
+        
+        #set the new end
+        prev.next = None
+        while curr:
+            nxt = curr.next
+            
+            #change links
+            curr.next = prev
+            prev = curr
+            curr = nxt 
+        
+        
+        return prev
