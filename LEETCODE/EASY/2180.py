4c77a96f6043549545d4e71df00e0d6bc2478acf
Avishek Arora <avishekarora@microsoft.com>
2022-04-27 12:22:02 +0530

    added few files.

--- LEETCODE/EASY/2180.py
+++ LEETCODE/EASY/2180.py
@@ -1,3 +1,7 @@
 class Solution:
     def countEven(self, num: int) -> int:
+        # if digit sum is odd 
+        # then the answer will be (num-1) // 2
+        # if digit is even then num // 2
+        # why 
         return (num-1) // 2 if sum(int(c) for c in str(num)) % 2 else num // 2
