cde5c8e4262679d076dfc937896207ca63cfcfea
avishek arora <avishek.arora@icloud.com>
2022-04-27 16:10:25 +0530

    fixed author

---
+++ LEETCODE/EASY/206.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head):
+        prev, curr = head, head.next if head and head.next else None
+        
+        # Edge # if list only contains single node
+        if not curr:
+            return head
+        
+        #set the new end
+        prev.next = None
+        while curr and curr.next:
+            nxt = curr.next
+            
+            #change links
+            curr.next = prev
+            prev = curr
+            curr = nxt 
+        
+        #process last node 
+        curr.next = prev
+        
+        return curr
+            
+            
+        
