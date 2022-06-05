3453c90db61655882ba6b502b8b4c8e5518c4d28
avishek arora <avishek.arora@icloud.com>
2022-06-06 00:00:25 +0530

    clean 2

--- CS50x-Coding-Contest-2017-hackerank/first.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	printf("coding_squad rocks\nCS50 rocks\n");
-	return 0;
-}
--- psets_of_cs50x/pset7/templates/quote_result.php
+++
@@ -1,6 +0,0 @@
-<div id="middle">
-    <h3> A share of <?php 
-        print ( $stock["name"] . $stock["symbol"] . " is of " . $stock["price"]) ; 
-    ?> </h3>
-
-</div>
